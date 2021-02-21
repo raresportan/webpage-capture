711f177a17d6ba6af58c94900dc7d2f144f567b2
Rares Portan <rares.portan@3pillarglobal.com>
2021-02-21 11:42:41 +0200

    Improve error handling

--- capture.js
+++ capture.js
@@ -1,18 +1,44 @@
 const { performance } = require('perf_hooks');
 const captureWebsite = require('capture-website');
 
-(async () => {
-    const start = performance.now();
-    const site = 'https://livejs.netlify.com';
-    const pic = site.split('://')[1] + '.jpg';
-    await captureWebsite.file(site, pic, {
-        type: "jpeg",
-        overwrite: true,
-        quality: 0.8,
-        fullPage: true,
-        timeout: 10,
-        scaleFactor: 1,
-    });
+const usage = `
+The URL was not provided or is incorrect
 
-    console.log('Done in', ((performance.now() - start) / 1000).toPrecision(2) + 's');
-})();
+Example:
+node capture https://news.ycombinator.com/
+`;
+
+
+const url = process.argv[2];
+if (!url) {
+    console.log(usage);
+} else {
+    (async () => {
+        const start = performance.now();
+        let site = url;
+        if (!url.startsWith('http')) {
+            site = 'https://' + site;
+        }
+        if(url.endsWith('/')) {
+            site = site.substring(0, site.length - 1);
+        }
+        const pic = site.split('://')[1] + '.jpg';
+        try {
+            await captureWebsite.file(site, pic, {
+                type: "jpeg",
+                overwrite: true,
+                quality: 0.8,
+                fullPage: true,
+                timeout: 10,
+                scaleFactor: 1,
+            });
+
+            console.log('Done in', ((performance.now() - start) / 1000).toPrecision(2) + 's');
+        }
+        catch (err) {
+            console.log(usage);
+            process.exit();
+        }
+    })();
+}
+
